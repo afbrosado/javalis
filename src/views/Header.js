672c9fc83e71aac1c4479b91f3490261cca9d109
rosado <andre.rosado@4itfuture.com>
2022-12-23 00:05:55 +0000

    Fix images

--- src/views/Header.js
+++ src/views/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div style={{position: "absolute", top: "-50px", left: 0, width: "100%"}}>
       <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-        <img src="https://imgur.com/jSrp1VO" alt="Logo javalis" width={50} height={50} style={{marginRight: 10}}/>
+        <img src="./imgs/javalis_dourados.jpg" alt="Logo javalis" width={50} height={50} style={{marginRight: 10}}/>
         <Typography variant="subtitle1">Javalis Dourados</Typography>
       </div>
     </div>
